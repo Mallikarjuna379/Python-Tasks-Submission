1c2cc78f30b64874780573e6e6bb2dfdb44844cf
Mallikarjuna379 <mallikarjuna0637@gmail.com>
2025-05-27 22:08:57 +0530

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,4 @@
+#Task 2: Create a Personalized Greeting
+FIRST_NAME = input("Enter your first name: ")
+LAST_NAME = input("Enter your last name: ")
+print("Hello,", FIRST_NAME, LAST_NAME + "! Welcome to Python program")
---
+++ task1.py
@@ -0,0 +1,9 @@
+#Task 1: Perform Basic Mathematical Operations
+
+A=float(input("enter the first number: "))
+B=float(input("enter the second number: "))
+print("Addition: ",A+B)
+print("Subtraction: ",A-B)
+print("Multiplication : ",A*B)
+print("Division : ",A/B)
+
