f380451a7f8ef1914cc95a3dce37c8ecb14913b1
Mallikarjuna379 <mallikarjuna0637@gmail.com>
2025-05-28 19:46:52 +0530

    Add files via upload

---
+++ ASSIGNMENT-2 TASK1.py
@@ -0,0 +1,5 @@
+A=float(input("Enter the number: "))
+if A%2 ==1:
+    print(A,"is an odd number")
+else:
+    print(A,"is an even number")
---
+++ ASSIGNMENT-2 TASK2.py
@@ -0,0 +1,5 @@
+N=list(range(1,51))
+X=0
+for i in N:
+    X=i+X
+print(X)
